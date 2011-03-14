64adab3273276330bf03b373668adab01b3c65ed
Noah Hall <noah.t.hall@gmail.com>
2011-03-13 23:15:16 -0700

    changed model to rely on single table

--- zombify/admin.py
+++ zombify/admin.py
@@ -1,10 +1,11 @@
-from zombify.models import Zomburl, Stats
+from zombify.models import Zomburl
 from django.contrib import admin
 
-admin.site.register(Zomburl)
+class ZombAdmin(admin.ModelAdmin):
+    fields = ['zomblink', 'destination_url', 'added', 'last_accessed', 'hits']
+    list_filter = ['added', 'last_accessed']
 
-class StatsAdmin(admin.ModelAdmin):
-    fields = ['zomblink', 'hits', 'added', 'last_accessed']
+admin.site.register(Zomburl, ZombAdmin)
 
-admin.site.register(Stats, StatsAdmin)
+
 
--- zombify/models.py
+++ zombify/models.py
@@ -3,16 +3,10 @@ from django.db import models
 class Zomburl(models.Model):
     zomblink = models.CharField(max_length=16)
     destination_url = models.CharField(max_length=200)
-    
-    def __unicode__(self):
-        return self.zomblink + ' redirects to ' + self.destination_url
-
-
-class Stats(models.Model):
-    zomblink = models.ForeignKey(Zomburl)
     added = models.DateTimeField('timestamp of url creation')
     last_accessed = models.DateTimeField('timestamp of last access')
     hits = models.IntegerField()
+
     def __unicode__(self):
-        return 'stats for zomblink ' + str(self.zomblink)
+        return self.zomblink + ' redirects to ' + self.destination_url
 
