966ae3ea0593b84e8af8e300d78aa4d1a978e673
Noah Hall <noah.t.hall@gmail.com>
2011-03-20 19:22:43 -0700

    renamed form object to match db field

--- zombify/views.py
+++ zombify/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render_to_response, get_object_or_404
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
 from zombify.models import Zomburl
 
 def index(request):
     return render_to_response('zombify/index.html')
 
-def result(request):
+def result(request, zomblink):
+# still building this... but at least now it theoretically checks for the csrf_token.
+# still need to build the view also.
+#    z = get_object_or_404(Zomburl, zomblink = zomblink)
+#    return render_to_response('zombify/result.html', {'result': z}, context_instance=RequestContext(request))    
     return HttpResponse("here's the results page placeholder! PLACEHOLD'd!!!")
 
 def redirect(request, zomblink):
