e23a7d718fa1ce62a4dd99cd23fb400cdff14e11
Noah Hall <noah.t.hall@gmail.com>
2011-05-08 03:29:11 -0700

    changed a var name in wsgi.py so uWSGI can hopefully find it now

--- wsgi.py
+++ wsgi.py
@@ -4,8 +4,9 @@ sys.path.append('/home/dotcloud/current/shortener')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'shortener.settings'
 os.environ['TZ'] = 'America/Los_Angeles'
 import django.core.handlers.wsgi
-djangoapplication = django.core.handlers.wsgi.WSGIHandler()
+application = django.core.handlers.wsgi.WSGIHandler()
 def application(environ, start_response):
     if 'SCRIPT_NAME' in environ:
                 del environ['SCRIPT_NAME']
                     return djangoapplication(environ, start_response)
+
