6877c848cfdd8324ea3f23ee7a3385bb1697560d
Noah Hall <noah.t.hall@gmail.com>
2011-05-14 14:15:49 -0700

    sayin' ayyyoo

--- wsgi.py
+++ wsgi.py
@@ -4,9 +4,9 @@ sys.path.append('/home/dotcloud/current/shortener')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'shortener.settings'
 os.environ['TZ'] = 'America/Los_Angeles'
 import django.core.handlers.wsgi
-application = django.core.handlers.wsgi.WSGIHandler()
-#def application(environ, start_response):
-#    if 'SCRIPT_NAME' in environ:
-#        del environ['SCRIPT_NAME']
-#    return application(environ, start_response)
+djangoapplication = django.core.handlers.wsgi.WSGIHandler()
+def application(environ, start_response):
+    if 'SCRIPT_NAME' in environ:
+        del environ['SCRIPT_NAME']
+    return application(environ, start_response)
 
