07333707e711080799f5004059f88cfa5af4384a
Noah Hall <noah.t.hall@gmail.com>
2011-05-14 14:14:15 -0700

    i throw my hands up in the air sometimes

--- wsgi.py
+++ wsgi.py
@@ -1,13 +1,12 @@
 import os
 import sys
 sys.path.append('/home/dotcloud/current/shortener')
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'shortener')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'shortener.settings'
 os.environ['TZ'] = 'America/Los_Angeles'
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
-def application(environ, start_response):
-    if 'SCRIPT_NAME' in environ:
-        del environ['SCRIPT_NAME']
-    return application(environ, start_response)
+#def application(environ, start_response):
+#    if 'SCRIPT_NAME' in environ:
+#        del environ['SCRIPT_NAME']
+#    return application(environ, start_response)
 
