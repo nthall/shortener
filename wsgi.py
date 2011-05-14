364e3c5821c1b504b578df2a9e0b0bdaa4018e9c
Noah Hall <noah.t.hall@gmail.com>
2011-05-14 14:11:56 -0700

    more hand-waving for the nginx gods...

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,7 @@
 import os
 import sys
 sys.path.append('/home/dotcloud/current/shortener')
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'shortener')))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'shortener.settings'
 os.environ['TZ'] = 'America/Los_Angeles'
 import django.core.handlers.wsgi
@@ -8,5 +9,5 @@ application = django.core.handlers.wsgi.WSGIHandler()
 def application(environ, start_response):
     if 'SCRIPT_NAME' in environ:
         del environ['SCRIPT_NAME']
-        return application(environ, start_response)
+    return application(environ, start_response)
 
