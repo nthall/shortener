86e221310c3c8667406a738e8ef1f81589cb71f3
Noah Hall <noah.t.hall@gmail.com>
2011-05-21 13:33:50 -0700

    changed urls.py in hopes of making admin accessible again...

--- shortener/urls.py
+++ shortener/urls.py
@@ -7,6 +7,6 @@ admin.autodiscover()
 urlpatterns = patterns('',
     (r'^$', 'shorten.views.index'),
     (r'^result$', 'shorten.views.result'),
-    (r'^admin/', include(admin.site.urls)),
+    (r'^admin$', include(admin.site.urls)),
     (r'^(?P<code>\w+)$', 'shorten.views.send_away'),
 )
