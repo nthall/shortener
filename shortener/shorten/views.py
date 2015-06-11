13d1b29a27b4a033de7eab0d104e1c0bb8b29812
root <noah.t.hall@gmail.com>
2015-06-11 20:01:49 +0000

    shorten max line length in views file

--- shortener/shorten/views.py
+++ shortener/shorten/views.py
@@ -6,16 +6,18 @@ from django.template import Context, loader, RequestContext
 from shorten.models import UrlPair
 
 def generate_code():
-    pop = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890123456789'
+    pop = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ' * 2 + '01234567890123456789'
     candidate = ''.join(random.sample(pop, 5))
     return candidate
 
 def check_code(candidate):
     code = candidate
-    try: match = UrlPair.objects.get(code__exact=code)
+    try: 
+        match = UrlPair.objects.get(code__exact=code)
     except (KeyError, UrlPair.DoesNotExist):
         return code
-    else: return False
+    else: 
+        return False
 
 def new_code():
     code = ''
@@ -23,31 +25,13 @@ def new_code():
         code = check_code(generate_code())
     return code
 
-#def make_url(request):
-#    try:
-#        dest_url = request.POST['destination_url']
-#    except (KeyError, UrlPair.DoesNotExist):
-#        render_to_response('shorten/index.html', {'error_message': "Looks like you didn't enter a valid link. If at first you don't succeed..."}) 
-#    else:
-#        try:
-#            match = UrlPair.objects.get(destination_url__iexact=dest_url)
-#        except (KeyError, UrlPair.DoesNotExist):
-#            new_url = UrlPair(code=new_code(), destination_url = dest_url, added=datetime.now())
-#            new_url.save()
-#            result(request, new_url)
-#        else:
-#            result(request, match, 1)
-
 def index(request):
     return render_to_response('index.html', context_instance=RequestContext(request))
 
 def result(request):
-    try:
+    if request.POST['destination_url']:
         dest_url = request.POST['destination_url']
-    except:
-        return render_to_response('index.html', {'error_message': "Looks like you didn't enter a valid link. If at first you don't succeed..."}, context_instance=RequestContext(request)) 
-    else:
-      #todo: check for empty first, yeesh. and smarter protocol check.
+      #todo: smarter protocol check.
         if dest_url[:7] != 'http://':
             dest_url = 'http://' + dest_url
         try:
@@ -59,20 +43,28 @@ def result(request):
         except:
             raise Http404
         else:
-            return render_to_response('result.html', {'url': match, 'alert': "This url has already been shortened! I guess great minds link alike."}) 
+            data = {'url': match, 'alert': "This url has already been shortened! I guess great minds link alike."}
+            return render_to_response('result.html', data) 
+    else:
+        data = {'error_message': "Looks like you didn't enter a valid link. If at first you don't succeed..."}
+        return render_to_response('index.html', data, context_instance=RequestContext(request)) 
+    else:
     
 
 def send_away(request, code):
     try:
         u = UrlPair.objects.get(code__exact=code)
     except (KeyError, UrlPair.DoesNotExist):
-        return render_to_response('index.html', {'error_message': "Oops, that's not a valid shortcode. Maybe you'd like to shorten something else?"}, context_instance=RequestContext(request))
+        data = {'error_message': "Oops, that's not a valid shortlink. Maybe you'd like to shorten something else?"}
+        return render_to_response('index.html', data, context_instance=RequestContext(request))
     except:
         raise Http404
     else:
         u.last_accessed = datetime.now()
-        try: u.hits = u.hits + 1
-        except: u.hits = 1
+        try: 
+            u.hits = u.hits + 1
+        except: 
+            u.hits = 1
         u.save()
         return redirect(u.destination_url)
 
